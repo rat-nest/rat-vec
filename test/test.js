78512df2949890465ba58e79ae09f3046f24af8b
mikolalysenko <mikolalysenko@gmail.com>
2015-05-07 22:58:34 -0500

    fix up constructor

--- test/test.js
+++ test/test.js
@@ -1,7 +1,18 @@
 'use strict'
 
 var tape = require('tape')
+var rv = require('../index')
+var round = require('../to-float')
 
 tape('exact rational', function(t) {
+
+  function verify(v) {
+    t.same(round(rv(v)), v)
+  }
+  verify([1])
+  verify([0.5])
+  verify([2])
+  verify([2, 4])
+
   t.end()
 })
