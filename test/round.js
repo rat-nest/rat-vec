4c74943bfb6d720eba09b71777e34282bce8c9d5
mikolalysenko <mikolalysenko@gmail.com>
2015-05-08 15:53:19 -0500

    more test cases

--- test/round.js
+++ test/round.js
@@ -16,13 +16,19 @@ tape('round-rational', function(t) {
   t.equals(roundRat(new BN(-1), new BN(-1)), 1)
   t.equals(roundRat(new BN(1), new BN(-4)), -0.25)
 
+  var pattern = (new BN(1)).add((new BN(1)).shln(32)).add((new BN(1)).shln(52))
+
   for(var i=0; i<1075+52; ++i) {
     var numer = new BN(1)
     var denom = (new BN(1)).shln(i)
     t.equals(roundRat(numer, denom), Math.pow(2,-i), 'n=2^-' + i)
 
     var xnumer = numer.add((new BN(1)).shln(52))
-    t.equals(roundRat(xnumer, denom), Math.pow(2,-i) + Math.pow(2,52-i), 'n=2^-' + i + '+2^'+(i-52))
+    t.equals(roundRat(xnumer, denom), Math.pow(2,-i) + Math.pow(2,52-i), 'n=2^-' + i + '+2^'+(52-i))
+
+    var ynumer = numer.add((new BN(1)).shln(26))
+    t.equals(roundRat(ynumer, denom), Math.pow(2, -i) + Math.pow(2, 26-i), 'n=2^-' + i + '+2^'+(26-i))
+    t.equals(roundRat(pattern, denom), Math.pow(2, -i) + Math.pow(2, 32-i) + Math.pow(2,52-i))
   }
 
   for(var i=0; i<1024; ++i) {
