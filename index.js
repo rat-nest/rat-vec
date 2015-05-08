5434756000dd8d2bde5fcb062285118386c2fece
mikolalysenko <mikolalysenko@gmail.com>
2015-05-07 23:40:54 -0500

    fix up a few more cases

--- index.js
+++ index.js
@@ -2,22 +2,22 @@
 
 module.exports = float2rat
 
-var BN = require('bn.js')
+var bn = require('./lib/bn')
 var db = require('double-bits')
 
 function float2rat(v) {
   var n = v.length
-  var lcm = 0
+  var lcm = -53
   for(var i=0; i<n; ++i) {
     if(v[i]) {
-      lcm = Math.min(lcm, db.exponent(v[i]))
+      lcm = Math.min(lcm, db.exponent(v[i]) - 53)
     }
   }
   var r = new Array(n+1)
   var f = Math.pow(2, -lcm)
   for(var i=0; i<n; ++i) {
-    r[i] = new BN(v[i]*f)
+    r[i] = bn(v[i]*f)
   }
-  r[n] = new BN(f)
+  r[n] = bn(f)
   return r
 }
--- test/ctor.js
+++ test/ctor.js
@@ -9,10 +9,13 @@ tape('exact rational', function(t) {
   function verify(v) {
     t.same(round(rv(v)), v)
   }
+  verify([1.5])
+  verify([Math.pow(2, 500)])
   verify([1])
   verify([0.5])
   verify([2])
   verify([2, 4])
-
+  verify([1, 1.5])
+  
   t.end()
 })
