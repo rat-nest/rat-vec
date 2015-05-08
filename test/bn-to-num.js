558bbb0ea98c077469ddb3c76e56a990b1cecfb7
Elijah Insua <tmpvar@gmail.com>
2015-05-08 01:40:55 -0400

    handle two words in bn-to-num

--- test/bn-to-num.js
+++ test/bn-to-num.js
@@ -6,4 +6,11 @@ test('single word', function(t) {
   t.equal(toFloat(new bn(-1234567)), -1234567, 'negative');
   t.equal(toFloat(new bn(1234567)), 1234567, 'positive');
   t.end();
-})
+});
+
+test('two words', function(t) {
+  var v = 12345671234567;
+  t.equal(toFloat(new bn(v)), v, 'positive');
+  t.equal(toFloat(new bn(-v)), -v, 'negative');
+  t.end();
+});
