48af7feb1a996ad94395ca86089687a6601299a0
mikolalysenko <mikolalysenko@gmail.com>
2015-05-07 23:00:39 -0500

    rename test cases, push old fix

--- index.js
+++ index.js
@@ -7,19 +7,17 @@ var db = require('double-bits')
 
 function float2rat(v) {
   var n = v.length
-  var lcm = Infinity
+  var lcm = 0
   for(var i=0; i<n; ++i) {
     if(v[i]) {
       lcm = Math.min(lcm, db.exponent(v[i]))
     }
   }
-  if(lcm >= Infinity) {
-    lcm = 1
-  }
   var r = new Array(n+1)
+  var f = Math.pow(2, -lcm)
   for(var i=0; i<n; ++i) {
-    r[i] = new BN(v[i]/lcm)
+    r[i] = new BN(v[i]*f)
   }
-  r[n] = new BN(lcm)
+  r[n] = new BN(f)
   return r
 }
