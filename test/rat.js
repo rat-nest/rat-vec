1da15450bc6f470ec9ca3408e6bad567d81a035e
mikolalysenko <mikolalysenko@gmail.com>
2015-05-07 22:45:29 -0500

    add some more test cases

--- test/rat.js
+++ test/rat.js
@@ -8,6 +8,12 @@ tape('round-rational', function(t) {
 
   t.equals(roundRat(new BN(1), new BN(1)), 1)
   t.equals(roundRat(new BN(1), new BN(2)), 0.5)
+  t.equals(roundRat(new BN(0), new BN(1)), 0)
+  t.equals(roundRat(new BN(-1), new BN(1)), -1)
+  t.equals(roundRat(new BN(1), new BN(-1)), -1)
+  t.equals(roundRat(new BN(-1), new BN(-1)), 1)
+  t.equals(roundRat(new BN(1), new BN(-4)), -0.25)
+
 
   t.end()
 })
