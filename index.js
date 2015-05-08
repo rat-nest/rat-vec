d459384222c240447551a7f2df918f9685a39c99
mikolalysenko <mikolalysenko@gmail.com>
2015-05-08 16:30:05 -0500

    fix up bugs in constructor

--- index.js
+++ index.js
@@ -2,23 +2,42 @@
 
 module.exports = float2rat
 
-var bn = require('./lib/bn')
+var BN = require('bn.js')
+var bn = require('./lib/num-to-bn')
 var db = require('double-bits')
 var rationalize = require('./rationalize')
 
 function float2rat(v) {
   var n = v.length
-  var lcm = -53
+  var lcm = 0
   for(var i=0; i<n; ++i) {
     if(v[i]) {
-      lcm = Math.min(lcm, db.exponent(v[i]) - 53)
+      lcm = Math.min(lcm, db.exponent(v[i])-53)
     }
   }
   var r = new Array(n+1)
-  var f = Math.pow(2, -lcm)
-  for(var i=0; i<n; ++i) {
-    r[i] = bn(v[i]*f)
+  if(lcm < -1023) {
+    //Handle the case where one of the components is a denormal
+    for(var i=0; i<n; ++i) {
+      var vf = Math.abs(v[i])
+      if(vf === 0) {
+        r[i] = new BN(0)
+      } else if(vf < Math.pow(2, -80)) {
+        var vv = v[i] * Math.pow(2, 540)
+        r[i] = bn(vv * Math.pow(2, 540))
+      } else if(vf > Math.pow(2, 53)) {
+        r[i] = bn(vf).shln(1080)
+      } else {
+        r[i] = bn(v[i] * Math.pow(2, 540)).shln(540)
+      }
+    }
+    r[n] = (new BN(1)).shln(1080)
+  } else {
+    var f = Math.pow(2, -lcm)
+    for(var i=0; i<n; ++i) {
+      r[i] = bn(v[i]*f)
+    }
+    r[n] = bn(f)
   }
-  r[n] = bn(f)
   return rationalize(r)
 }
--- test/ctor.js
+++ test/ctor.js
@@ -9,6 +9,12 @@ tape('exact rational', function(t) {
   function verify(v) {
     t.same(round(rv(v)), v)
   }
+
+  verify([Math.pow(2,1023)])
+  verify([Math.pow(2,-1074)])
+  verify([Math.pow(2,-1074), Math.pow(2,1023)])
+
+  verify([0.1])
   verify([1.5])
   verify([Math.pow(2, 500)])
   verify([1])
@@ -16,6 +22,27 @@ tape('exact rational', function(t) {
   verify([2])
   verify([2, 4])
   verify([1, 1.5])
-  
+  verify([0.1])
+  verify([0.3])
+  verify([0.7])
+  verify([0.3, 0.9])
+  verify([1/3])
+
+  //across multiple scales
+  for(var i=-1074; i<1024; ++i) {
+    var x = Math.pow(2, i)
+    verify([x])
+    verify([x + Math.pow(2,i-52)])
+    verify([x*Math.random()])
+    for(var j=-1070; j<1024; ++j) {
+      var y = Math.pow(2, j)
+      verify([x, y])
+      verify([x + Math.pow(2,i-52), y])
+      verify([x, y + Math.pow(2,j-52)])
+      verify([x + Math.pow(2,i-52), y + Math.pow(2,j-52)])
+      verify([x*Math.random(), y*Math.random()])
+    }
+  }
+
   t.end()
 })
