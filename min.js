ef2f595d7c251ae00a71bd2a0a92ebb61cb4a134
mikolalysenko <mikolalysenko@gmail.com>
2015-05-12 17:14:27 -0400

    add max/min component-wise functions

---
+++ min.js
@@ -0,0 +1,23 @@
+'use strict'
+
+var rationalize = require('./rationalize')
+
+module.exports = min
+
+function min(a,b) {
+  var n = a.length-1
+  var r = new Array(n+1)
+  var aw = a[n]
+  var bw = b[n]
+  for(var i=0; i<n; ++i) {
+    var ai = a[i].mul(bw)
+    var bi = b[i].mul(aw)
+    if(ai.cmp(bi) > 0) {
+      r[i] = bi
+    } else {
+      r[i] = ai
+    }
+  }
+  r[n] = aw.mul(bw)
+  return rationalize(r)
+}
