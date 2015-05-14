63a852c980d03ad0d6f21be625456bf8dd997f97
Elijah Insua <tmpvar@gmail.com>
2015-05-13 23:54:49 -0400

    woops, use equal instead of cmp

--- test/index.js
+++ test/index.js
@@ -1,11 +1,11 @@
 var test = require('tape');
-var cmp = require('../cmp')
+var equal = require('../equal')
 var rv = require('../index');
 
 test('(1/5) == 1/5', function(t) {
   var a = rv([1/5]);
   var b = rv([1/5]);
 
-  t.deepEqual(cmp(a, b), [0], '[0]');
+  t.deepEqual(equal(a, b), true, '[0]');
   t.end();
 });
