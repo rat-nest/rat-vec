bd79077c596f18c9a69b4fe15ae79a49cfd52958
mikolalysenko <mikolalysenko@gmail.com>
2017-01-06 07:26:39 -1000

    bump version of big-rat

--- add.js
+++ add.js
@@ -4,10 +4,10 @@ var bnadd = require('big-rat/add')
 
 module.exports = add
 
-function add(a, b) {
+function add (a, b) {
   var n = a.length
   var r = new Array(n)
-    for(var i=0; i<n; ++i) {
+  for (var i=0; i<n; ++i) {
     r[i] = bnadd(a[i], b[i])
   }
   return r
